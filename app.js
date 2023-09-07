6cd2bdaf94bb5ba1b5ca24ac92e8034c9027b832
Aleksandra <143549049+Muzychenko-qa@users.noreply.github.com>
2023-09-07 23:07:14 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xff5a00,
+  backgroundColor: 0x2d0e0e,
+  points: 20.00,
+  maxDistance: 30.00,
+  showDots: false
+})
